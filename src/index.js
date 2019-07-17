856275fb0d46049af262f9b36ea5d4a0a3ee6b62
Nataly Romanenko <nataliia.romanenko@gmail.com>
2019-07-17 15:52:18 +0300

    project refactoring

--- src/index.js
+++ src/index.js
@@ -1,36 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-import ToDoList from "./components/todo-list";
-import ItemStatusFilter from "./components/item-status-filter";
-import './index.css'
 
-const App = ()=>{
-    const isLoggined = true;
-    const loginBox = <p>Log in, please</p>;
-    const welcomeBox = <p>Welcome Back</p>;
+import App from "./components/app";
 
-    const todoData = [
-        {id: 1, label: 'Drink coffee', important: false},
-        {id: 2, label: 'Make Awesome', important: true},
-        {id: 3, label: 'Have a lunch', important: false},
-    ];
 
-    return (
-        <div className="todo-app">
-            <span>{(new Date()).toString()}</span>
-            { isLoggined ? welcomeBox : loginBox }
-
-            <AppHeader  toDo={1} done={3}/>
-            <div className="top-panel d-flex">
-                <SearchPanel/>
-                <ItemStatusFilter />
-            </div>
-            <ToDoList todos = {todoData}/>
-    </div>
-    )
-};
-
-    ReactDom.render(<App/>,
-        document.getElementById('root'));
+ReactDom.render(<App/>,
+    document.getElementById('root'));
